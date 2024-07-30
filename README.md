4e91732581d96bbd3fff6107b5a5311e1b843921
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-07-30 14:37:47 +0300

    clone 2048 game

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-This project is game 2048.
+<h4>This project is game 2048.</h4>
 
-Technologies that were used: HTML, CSS and JS.
+In this game, the player must combine cells containing the same numbers until they reach the number 2048. The cells can contain only integer values starting from 2, and that are a power of two, like 2, 4, 8, 16, and so on.
+
+The board has dimension of 4 x 4 cells, so that it can fit up to 16 cells. If the board is full, and there is no possible move to make like merging cells together - the game is over.
+
+<h4>Technologies that were used:</h4>
+<p>HTML - was used to build semantically correct page.</p>
+<p>SCSS - was used for styling.</p>
+<p>JavaScript - was used to add interactive behavior to web application.</p>
 
 Link to the preview - [DEMO LINK](https://vinogradova8.github.io/2048_game/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-inner-declarations */
-'use strict';
+"use strict";
 
-const button = document.querySelector('button');
-const messageStart = document.querySelector('.message-start');
-const messageWin = document.querySelector('.message-win');
-const messageLose = document.querySelector('.message-lose');
+const button = document.querySelector("button");
+const messageStart = document.querySelector(".message-start");
+const messageWin = document.querySelector(".message-win");
+const messageLose = document.querySelector(".message-lose");
 
-const fieldRows = document.querySelectorAll('.field-row');
+const fieldRows = document.querySelectorAll(".field-row");
 
 const rowsArr = [...fieldRows];
 
-const gameScore = document.querySelector('.game-score');
+const gameScore = document.querySelector(".game-score");
 
 gameScore.innerText = 0;
 
-const gameField = document.querySelector('.game-field');
+const gameField = document.querySelector(".game-field");
 
 let gameOver = 0;
 
-const cells = document.querySelectorAll('.field-cell');
+const cells = document.querySelectorAll(".field-cell");
 const cellsArr = [...cells];
 
 const columnsArr = [];
@@ -54,8 +54,8 @@ function chooseCell() {
 
   let fieldCell = document.querySelector(`.field-cell--n${fieldCellNumber}`);
 
-  while (!(fieldCell.innerText === '')) {
-    fieldCellNumber = 1 + Math.ceil(Math.random() * 15);
+  while (!(fieldCell.innerText === "")) {
+    fieldCellNumber = randomNumber(1, 16);
 
     fieldCell = document.querySelector(`.field-cell--n${fieldCellNumber}`);
   }
@@ -64,18 +64,18 @@ function chooseCell() {
 }
 
 function colorCells() {
-  const cellList = document.querySelectorAll('.field-cell');
+  const cellList = document.querySelectorAll(".field-cell");
 
   const cellArr = [...cellList];
 
   cellArr.forEach((cell) => {
-    if (cell.innerText === '') {
+    if (cell.innerText === "") {
       for (let i = 2; i <= 2048; i *= 2) {
         if (cell.classList.contains(`field-cell--${i}`)) {
           cell.classList.remove(`field-cell--${i}`);
         }
       }
-      cell.classList.add('field-cell--0');
+      cell.classList.add("field-cell--0");
     } else {
       if (cell.classList.contains(`field-cell--0`)) {
         cell.classList.remove(`field-cell--0`);
@@ -94,19 +94,19 @@ function colorCells() {
 function addNewNumber() {
   const cellContent = chooseNumber();
 
-  const fieldCell1 = chooseCell();
+  const fieldCell = chooseCell();
 
   setTimeout(() => {
-    fieldCell1.classList.add(`field-cell--${cellContent}`);
-    fieldCell1.innerText = cellContent;
+    fieldCell.classList.add(`field-cell--${cellContent}`);
+    fieldCell.innerText = cellContent;
   }, 100);
 }
 
 function emptyCellCheckRightAndDown(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
-    if (arr[i].innerText === '') {
+    if (arr[i].innerText === "") {
       arr[i].innerText = arr[i - 1].innerText;
-      arr[i - 1].innerText = '';
+      arr[i - 1].innerText = "";
     } else {
       continue;
     }
@@ -115,9 +115,9 @@ function emptyCellCheckRightAndDown(arr) {
 
 function emptyCellCheckLeftAndUp(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i].innerText === '') {
+    if (arr[i].innerText === "") {
       arr[i].innerText = arr[i + 1].innerText;
-      arr[i + 1].innerText = '';
+      arr[i + 1].innerText = "";
     } else {
       continue;
     }
@@ -131,16 +131,16 @@ function emptyCellInRowOrColumnCheck(arr, keyCode) {
     arrOfvalues.push(ch.innerText);
   }
 
-  if (!arrOfvalues.includes('')) {
+  if (!arrOfvalues.includes("")) {
     return;
   }
 
-  if (keyCode === 'ArrowRight' || keyCode === 'ArrowDown') {
+  if (keyCode === "ArrowRight" || keyCode === "ArrowDown") {
     emptyCellCheckRightAndDown(arr);
     emptyCellCheckRightAndDown(arr);
   }
 
-  if (keyCode === 'ArrowLeft' || keyCode === 'ArrowUp') {
+  if (keyCode === "ArrowLeft" || keyCode === "ArrowUp") {
     emptyCellCheckLeftAndUp(arr);
     emptyCellCheckLeftAndUp(arr);
   }
@@ -152,9 +152,9 @@ function emptyCellCheck() {
   for (const ch of rowsArr) {
     for (let i = 0; i < 3; i++) {
       if (
-        ch.children[i].innerText !== ch.children[i + 1].innerText
-        && ch.children[i].innerText !== ''
-        && ch.children[i + 1].innerText !== ''
+        ch.children[i].innerText !== ch.children[i + 1].innerText &&
+        ch.children[i].innerText !== "" &&
+        ch.children[i + 1].innerText !== ""
       ) {
         count++;
       }
@@ -164,9 +164,9 @@ function emptyCellCheck() {
   for (const ch of columnsArr) {
     for (let i = 0; i < 3; i++) {
       if (
-        ch[i].innerText !== ch[i + 1].innerText
-        && ch[i].innerText !== ''
-        && ch[i + 1].innerText !== ''
+        ch[i].innerText !== ch[i + 1].innerText &&
+        ch[i].innerText !== "" &&
+        ch[i + 1].innerText !== ""
       ) {
         count++;
       }
@@ -176,49 +176,49 @@ function emptyCellCheck() {
   const innerTextArr = cellsArr.map((cell) => cell.innerText);
 
   if (count === 24) {
-    messageLose.classList.remove('hidden');
-    gameField.classList.add('game-over');
+    messageLose.classList.remove("hidden");
+    gameField.classList.add("game-over");
     gameOver++;
   }
 
-  if (innerTextArr.includes('2048')) {
-    messageWin.classList.remove('hidden');
-    gameField.classList.add('game-over');
+  if (innerTextArr.includes("2048")) {
+    messageWin.classList.remove("hidden");
+    gameField.classList.add("game-over");
     gameOver++;
   }
 
   if (gameOver > 0 && count === 24) {
-    gameField.classList.add('game-over');
-    messageLose.classList.remove('hidden');
+    gameField.classList.add("game-over");
+    messageLose.classList.remove("hidden");
   }
 }
 
 function clearCells() {
-  cellsArr.forEach(cell => {
-    cell.innerText = '';
+  cellsArr.forEach((cell) => {
+    cell.innerText = "";
   });
   colorCells();
 }
 
 colorCells();
 
-button.addEventListener('click', () => {
-  if (button.classList.contains('start')) {
-    button.innerText = 'Restart';
-    button.classList.remove('start');
-    button.classList.add('restart');
-    messageStart.classList.add('hidden');
+button.addEventListener("click", () => {
+  if (button.classList.contains("start")) {
+    button.innerText = "Restart";
+    button.classList.remove("start");
+    button.classList.add("restart");
+    messageStart.classList.add("hidden");
     addNewNumber();
     addNewNumber();
 
     return;
   }
 
-  if (button.classList.contains('restart')) {
+  if (button.classList.contains("restart")) {
     clearCells();
-    messageWin.classList.add('hidden');
-    messageLose.classList.add('hidden');
-    gameField.classList.remove('game-over');
+    messageWin.classList.add("hidden");
+    messageLose.classList.add("hidden");
+    gameField.classList.remove("game-over");
     gameScore.innerText = 0;
     gameOver = 0;
     addNewNumber();
@@ -226,8 +226,8 @@ button.addEventListener('click', () => {
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'ArrowRight') {
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight") {
     if (gameOver > 0) {
       return;
     }
@@ -239,16 +239,14 @@ document.addEventListener('keydown', (e) => {
         emptyCellInRowOrColumnCheck(ch.children, e.code);
 
         if (
-          ch.children[i].innerText === ch.children[i - 1].innerText
-          && ch.children[i].innerText !== ''
+          ch.children[i].innerText === ch.children[i - 1].innerText &&
+          ch.children[i].innerText !== ""
         ) {
-          ch.children[i].innerText = `${
-            +ch.children[i].innerText + +ch.children[i - 1].innerText
-          }`;
-          ch.children[i - 1].innerText = '';
+          ch.children[i].innerText = `${+ch.children[i].innerText * 2}`;
+          ch.children[i - 1].innerText = "";
 
-          gameScore.innerText
-            = +gameScore.innerText + +ch.children[i].innerText;
+          gameScore.innerText =
+            +gameScore.innerText + +ch.children[i].innerText;
         } else {
           continue;
         }
@@ -259,20 +257,18 @@ document.addEventListener('keydown', (e) => {
     const numbersArrAfter = cellsArr.map((cell) => cell.innerText);
 
     if (
-      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter)
-      && gameOver === 0
+      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter) &&
+      gameOver === 0
     ) {
       addNewNumber();
     }
 
-    setTimeout(() => {
-      emptyCellCheck();
-    }, 100);
+    emptyCellCheck();
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'ArrowDown') {
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowDown") {
     if (gameOver > 0) {
       return;
     }
@@ -283,9 +279,9 @@ document.addEventListener('keydown', (e) => {
       for (let i = ch.length - 1; i > 0; i--) {
         emptyCellInRowOrColumnCheck(ch, e.code);
 
-        if (ch[i].innerText === ch[i - 1].innerText && ch[i].innerText !== '') {
-          ch[i].innerText = `${+ch[i].innerText + +ch[i - 1].innerText}`;
-          ch[i - 1].innerText = '';
+        if (ch[i].innerText === ch[i - 1].innerText && ch[i].innerText !== "") {
+          ch[i].innerText = `${+ch[i].innerText * 2}`;
+          ch[i - 1].innerText = "";
 
           gameScore.innerText = +gameScore.innerText + +ch[i].innerText;
         } else {
@@ -298,20 +294,18 @@ document.addEventListener('keydown', (e) => {
     const numbersArrAfter = cellsArr.map((cell) => cell.innerText);
 
     if (
-      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter)
-      && gameOver === 0
+      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter) &&
+      gameOver === 0
     ) {
       addNewNumber();
     }
 
-    setTimeout(() => {
-      emptyCellCheck();
-    }, 100);
+    emptyCellCheck();
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'ArrowLeft') {
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft") {
     if (gameOver > 0) {
       return;
     }
@@ -323,16 +317,14 @@ document.addEventListener('keydown', (e) => {
         emptyCellInRowOrColumnCheck(ch.children, e.code);
 
         if (
-          ch.children[i].innerText === ch.children[i + 1].innerText
-          && ch.children[i].innerText !== ''
+          ch.children[i].innerText === ch.children[i + 1].innerText &&
+          ch.children[i].innerText !== ""
         ) {
-          ch.children[i].innerText = `${
-            +ch.children[i + 1].innerText + +ch.children[i].innerText
-          }`;
-          ch.children[i + 1].innerText = '';
+          ch.children[i].innerText = `${+ch.children[i + 1].innerText * 2}`;
+          ch.children[i + 1].innerText = "";
 
-          gameScore.innerText
-            = +gameScore.innerText + +ch.children[i].innerText;
+          gameScore.innerText =
+            +gameScore.innerText + +ch.children[i].innerText;
         } else {
           continue;
         }
@@ -343,20 +335,18 @@ document.addEventListener('keydown', (e) => {
     const numbersArrAfter = cellsArr.map((cell) => cell.innerText);
 
     if (
-      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter)
-      && gameOver === 0
+      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter) &&
+      gameOver === 0
     ) {
       addNewNumber();
     }
 
-    setTimeout(() => {
-      emptyCellCheck();
-    }, 100);
+    emptyCellCheck();
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'ArrowUp') {
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowUp") {
     if (gameOver > 0) {
       return;
     }
@@ -367,9 +357,9 @@ document.addEventListener('keydown', (e) => {
       for (let i = 0; i < ch.length - 1; i++) {
         emptyCellInRowOrColumnCheck(ch, e.code);
 
-        if (ch[i].innerText === ch[i + 1].innerText && ch[i].innerText !== '') {
-          ch[i].innerText = `${+ch[i + 1].innerText + +ch[i].innerText}`;
-          ch[i + 1].innerText = '';
+        if (ch[i].innerText === ch[i + 1].innerText && ch[i].innerText !== "") {
+          ch[i].innerText = `${+ch[i + 1].innerText * 2}`;
+          ch[i + 1].innerText = "";
 
           gameScore.innerText = +gameScore.innerText + +ch[i].innerText;
         } else {
@@ -382,14 +372,12 @@ document.addEventListener('keydown', (e) => {
     const numbersArrAfter = cellsArr.map((cell) => cell.innerText);
 
     if (
-      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter)
-      && gameOver === 0
+      JSON.stringify(numbersArrBefore) !== JSON.stringify(numbersArrAfter) &&
+      gameOver === 0
     ) {
       addNewNumber();
     }
 
-    setTimeout(() => {
-      emptyCellCheck();
-    }, 100);
+    emptyCellCheck();
   }
 });
